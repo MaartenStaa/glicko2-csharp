13172212c6b940635ead5c29e06149fe739fd397
Maarten Staa <aistina@gmail.com>
2015-09-08 15:55:12 +0200

    Add source code

---
+++ Glicko2/src/RatingPeriodResults.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+
+namespace Glicko2
+{
+    /// <summary>
+    /// This class holds the results accumulated over a rating period.
+    /// </summary>
+    public class RatingPeriodResults
+    {
+        private readonly List<Result> _results = new List<Result>();
+        private readonly HashSet<Rating> _participants = new HashSet<Rating>();
+
+        /// <summary>
+        /// Create an empty result set.
+        /// </summary>
+        public RatingPeriodResults()
+        {
+        }
+
+        /// <summary>
+        /// Constructor that allows you to initialise the list of participants.
+        /// </summary>
+        /// <param name="participants"></param>
+        public RatingPeriodResults(HashSet<Rating> participants)
+        {
+            _participants = participants;
+        }
+
+        /// <summary>
+        /// Add a result to the set.
+        /// </summary>
+        /// <param name="winner"></param>
+        /// <param name="loser"></param>
+        public void AddResult(Rating winner, Rating loser)
+        {
+            var result = new Result(winner, loser);
+
+            _results.Add(result);
+        }
+
+        /// <summary>
+        /// Record a draw between two players and add to the set.
+        /// </summary>
+        /// <param name="player1"></param>
+        /// <param name="player2"></param>
+        public void AddDraw(Rating player1, Rating player2)
+        {
+            var result = new Result(player1, player2, true);
+
+            _results.Add(result);
+        }
+
+        /// <summary>
+        /// Get a list of the results for a given player.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public IList<Result> GetResults(Rating player)
+        {
+            var filteredResults = new List<Result>();
+
+            foreach (var result in _results)
+            {
+                if (result.Participated(player))
+                {
+                    filteredResults.Add(result);
+                }
+            }
+
+            return filteredResults;
+        }
+
+        /// <summary>
+        /// Get all the participants whose results are being tracked.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Rating> GetParticipants()
+        {
+            // Run through the results and make sure all players have been pushed into the participants set.
+            foreach (var result in _results)
+            {
+                _participants.Add(result.GetWinner());
+                _participants.Add(result.GetLoser());
+            }
+
+            return _participants;
+        }
+
+        /// <summary>
+        /// Add a participant to the rating period, e.g. so that their rating will
+        /// still be calculated even if they don't actually compete.
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddParticipants(Rating rating)
+        {
+            _participants.Add(rating);
+        }
+
+        /// <summary>
+        /// Clear the result set.
+        /// </summary>
+        public void Clear()
+        {
+            _results.Clear();
+        }
+    }
+}
